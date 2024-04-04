e2b2b998af4bf9086f1e710fb57a0716c142f69a
imMRcreeperMC <124432033+MrCreeperMC@users.noreply.github.com>
2024-04-04 04:15:46 +0330

    Rethemed and changed

--- src/main/java/me/lrxh/practice/setting/Settings.java
+++ src/main/java/me/lrxh/practice/setting/Settings.java
@@ -6,15 +6,15 @@ import org.bukkit.Material;
 @Getter
 public enum Settings {
 
-    SHOW_PLAYERS("Toggle Players Visibility", Material.COMPASS, "Show or Hide players."),
-    SHOW_SCOREBOARD("Toggle Scoreboard", Material.EYE_OF_ENDER, "Show or Hide Scoreboard."),
-    ALLOW_SPECTATORS("Toggle Spectators", Material.EGG, "Allow players to spectate."),
+    SHOW_PLAYERS("Toggle Players Visibility", Material.ENDER_PEARL, "Show or Hide players."),
+    SHOW_SCOREBOARD("Toggle Scoreboard", Material.PAINT, "Show or Hide Scoreboard."),
+    ALLOW_SPECTATORS("Toggle Spectators", Material.ENDER_EYE, "Allow players to spectate."),
     ALLOW_DUELS("Toggle Duels", Material.DIAMOND_SWORD, "Allow Duel Requests."),
     KILL_EFFECTS("Kill Effects", Material.DIAMOND_AXE, "Select Kill Effect."),
-    THEME("Select Theme", Material.BOOK, "Select Color Theme."),
-    PING_RANGE("Ping Range", Material.STICK, "Change Ping Range."),
-    TIME_CHANGE("Change Time", Material.WATCH, "Change Ping Range."),
-    MENU_SOUNDS("Menu Sounds", Material.REDSTONE_COMPARATOR, "Toggle Menu Sounds.");
+    THEME("Select Theme", Material.DYE, "Select Color Theme."),
+    PING_RANGE("Ping Range", Material.MAP, "Change Your Ping Range."),
+    TIME_CHANGE("Change Time", Material.WATCH, "Set Your Custom Time."),
+    MENU_SOUNDS("Menu Sounds", Material.BEACON, "Toggle The Menu Sounds.");
 
     private final String name;
     private final Material material;
